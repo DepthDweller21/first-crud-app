0089f63b97aadcfb3aad7f5db4fb3b4c98101a3f
Muhammad Haris <muhammadharis571@gmail.com>
2023-11-26 19:58:29 +0500

    added databse record creation and now is capable of sending quotes to my database in mongodb

--- server.js
+++ server.js
@@ -1,20 +1,57 @@
 const express=require('express')
 const parser=require('body-parser')
+const mongoClient=require('mongodb').MongoClient
 const app=express()
-const PORT=9001
+const connectionString = "mongodb+srv://DepthDweller21:P0YvljIktS2BbbVN@new-crud-app.ebjwiea.mongodb.net/"
+const PORT=8000
+
+// not sure what this is but I need it to do something related to json, parsing, urls and APIs, will find out soon enough
 
 app.use(parser.urlencoded({extended:true}))
 
-app.get('/',function(req,res){
-    res.sendFile(__dirname+'/index.html')
-})
-
-app.post('/quotes', (req, res) => {
-    console.log(req.body)
-})
+// this line of code uses express to launch the site on localhost PORT as shown in the variable section
 
 app.listen(PORT,function(){
     console.log(`server is live Mr.Duck on port: ${PORT}, please visit the project on localhost:${PORT}`)
 })
 
+//connecting to database
+async function runDatabase(){
+    try{
+        const client= await mongoClient.connect(connectionString)
+        console.log('Connected to Database Mr.Duck')
+        const db =client.db('crud-app')
+        quotesCollection=db.collection('quotes')
+    }catch(err){
+        console.error(err)
+    }
+}
+runDatabase()
 
+// all this till now was the application initialisation
+
+
+//here I listen for get requests for any files I might need to send from the server to the client
+
+app.get('/',function(req,res){
+    res.sendFile(__dirname+'/index.html')
+})
+
+//this line of code listens on path /quotes which the HTML responds on
+
+app.post('/quotes', async (req, res) => {
+    try{
+        const result = await quotesCollection.insertOne(req.body)
+        res.redirect('/')
+    }catch(err){
+        console.error(err)
+    }
+
+})
+
+
+
+
+
+
+//P0YvljIktS2BbbVN
